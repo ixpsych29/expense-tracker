6f342d67ff07f132b50298ccedd87c639186111b
Abdullah <abdullahbinrafique945@gmail.com>
2022-10-07 11:33:16 +0500

    Building Basic Components with some static data

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
+
+const ExpenseItem = (props) => {
+  return (
+    <Card className="expense-item">
+      {/* toISOString return object of Date as a string
+      <div> {props.date.toISOString()} </div> */}
+
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
