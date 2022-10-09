562d879349f46c6f6eedfb10f7aeb2dd5ff7585b
Abdullah <abdullahbinrafique945@gmail.com>
2022-10-09 16:37:37 +0500

    two-way binding concept on forms

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,17 +6,17 @@ export const ExpenseForm = () => {
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
 
-//using one state instead of individual states burh:
-//   const [userInput, setUserInput] = useState({
-//     enteredTitle: '',
-//     enteredAmount: '',
-//     enteredDate: '',
-//   });
+  //using one state instead of individual states burh:
+  //   const [userInput, setUserInput] = useState({
+  //     enteredTitle: '',
+  //     enteredAmount: '',
+  //     enteredDate: '',
+  //   });
 
   //declaration of functions
-  
+
   const titleChangeHandler = (event) => {
-    console.log(event.target.value);
+    // console.log(event.target.value);
     //1-way
     setEnteredTitle(event.target.value);
     //2-way
@@ -28,27 +28,28 @@ export const ExpenseForm = () => {
     // setUserInput((prevState) => {
     //     return {...userInput, enteredTitle: event.target.value};
     // });
-
   };
   const amountChangeHandler = (event) => {
-    console.log(event.target.value);
-    setEnteredAmount(event.target.value);    
+    // console.log(event.target.value);
+    setEnteredAmount(event.target.value);
   };
   const dateChangeHandler = (event) => {
-    console.log(event.target.value);
+    // console.log(event.target.value);
     setEnteredDate(event.target.value);
   };
   const submitHandler = (event) => {
     event.preventDefault();
-    console.log('submitted');
-
+    // console.log('submitted');
     const expenseData = {
       title: enteredTitle,
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
     console.log(expenseData);
-  }
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
 
   return (
     <div>
@@ -56,12 +57,17 @@ export const ExpenseForm = () => {
         <div className="new-expense__control">
           <div className="new-expense__control">
             <label>Title</label>
-            <input type="text" onChange={titleChangeHandler} />
+            <input
+              type="text"
+              value={enteredTitle}
+              onChange={titleChangeHandler}
+            />
           </div>
           <div className="new-expense__control">
             <label>Amount</label>
             <input
               type="number"
+              value={enteredAmount}
               min="0.01"
               step="0.01"
               onChange={amountChangeHandler}
@@ -71,6 +77,7 @@ export const ExpenseForm = () => {
             <label>Date</label>
             <input
               type="date"
+              value={enteredDate}
               min="2019-01-01"
               max="2022-12-31"
               onChange={dateChangeHandler}
