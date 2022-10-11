6b1a5563536a52309b148ce47f8a1deddccdeb04
Abdullah <abdullahbinrafique945@gmail.com>
2022-10-11 16:21:48 +0500

    Working on Add Expense Component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
 import { NewExpense } from "./components/NewExpense/NewExpense";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
