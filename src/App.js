80816311ee63527ad0b90aeda1c172f5f555f054
Abdullah <abdullahbinrafique945@gmail.com>
2022-10-11 15:42:51 +0500

    changes were made in app.js

--- src/App.js
+++ src/App.js
@@ -26,9 +26,14 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log('in App.js');
+    console.log(expense);
+  }
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,14 +2,15 @@ import React from "react";
 import "./NewExpense.css";
 import { ExpenseForm } from "./ExpenseForm";
 
-export const NewExpense = () => {
+export const NewExpense = (props) => {
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       //spread operator ...
       ...enteredExpenseData,
       id: Math.random().toString()
     };
-    console.log(expenseData);
+    // console.log(expenseData);
+    props.onAddExpense(expenseData);
   }
   return (
     <div className="new-expense">
