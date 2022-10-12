7c572a091f4743f05fb8eb9aa06ed34070f2074d
Abdullah <abdullahbinrafique945@gmail.com>
2022-10-12 15:13:33 +0500

    Minor changes

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,15 +5,17 @@ import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
   return (
-    <Card className="expense-item">
-      {/* toISOString return object of Date as a string
+    <li>
+      <Card className="expense-item">
+        {/* toISOString return object of Date as a string
       <div> {props.date.toISOString()} </div> */}
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-    </Card>
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
 };
 
