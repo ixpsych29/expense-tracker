610588d866792e4eb312daa36d394cbfb9161fb0
Abdullah <abdullahbinrafique945@gmail.com>
2022-10-12 14:28:37 +0500

    clean code

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,45 +15,26 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  let expensesContent = <p>No Expenses Found!</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {/* map creates a new array from existing array simpy copying all its data */}
-      {/* its works dynamically bruh */}
-      {filteredExpenses.length === 0 && <p>No Expenses Found! Add Some?</p>}
-      {filteredExpenses.length > 0 &&       //using && is like abusing bcz it checks before and if it is true it executes the code after &&
-        filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
-      ;{/* it works statically as hard coded bruh */}
-      {/* <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      /> */}
+      {expensesContent}
     </Card>
   );
 };
