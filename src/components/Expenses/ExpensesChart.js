5d636bc84590f93964ad81a36188dd4344a0cca7
Abdullah <abdullahbinrafique945@gmail.com>
2022-10-13 17:22:11 +0500

    clearing label error.

--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -3,18 +3,18 @@ import Chart from "../Chart/Chart";
 
 export const ExpensesChart = (props) => {
   const chartDataPoints = [
-    { key: "Jan", value: 0 },
-    { key: "Feb", value: 0 },
-    { key: "Mar", value: 0 },
-    { key: "Apr", value: 0 },
-    { key: "May", value: 0 },
-    { key: "Jun", value: 0 },
-    { key: "Jul", value: 0 },
-    { key: "Aug", value: 0 },
-    { key: "Sep", value: 0 },
-    { key: "Oct", value: 0 },
-    { key: "Nov", value: 0 },
-    { key: "Dec", value: 0 },
+    { label: "Jan", value: 0 },
+    { label: "Feb", value: 0 },
+    { label: "Mar", value: 0 },
+    { label: "Apr", value: 0 },
+    { label: "May", value: 0 },
+    { label: "Jun", value: 0 },
+    { label: "Jul", value: 0 },
+    { label: "Aug", value: 0 },
+    { label: "Sep", value: 0 },
+    { label: "Oct", value: 0 },
+    { label: "Nov", value: 0 },
+    { label: "Dec", value: 0 },
   ];
 
   for (const expense of props.expenses) {
