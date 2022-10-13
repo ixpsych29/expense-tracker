328069d00368598194460a9657e782ef319d8edf
Abdullah <abdullahbinrafique945@gmail.com>
2022-10-13 10:36:36 +0500

    minor changes

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
 import "./Expenses.css";
